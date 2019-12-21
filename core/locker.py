84a39b06d96343ebee3bb5e2e3eb2486c7973d6b
lilao <lilao@163.com>
2019-12-21 10:03:28 +0800

    remote the the lock if the sacred job is failed

--- core/locker.py
+++ core/locker.py
@@ -9,16 +9,25 @@ import functools
 from bson.objectid import ObjectId
 from datetime import datetime
 import socket
-
+from easydict import EasyDict as edict
 
 class task_locker:
-    def __init__(self, url, version, rollback=True):
+
+    def __init__(self, url, version, rollback=True, remove_failed=True):
+        """
+
+        :param url:
+        :param version:
+        :param rollback:
+        :param remove_failed: It only work when the task is manager by sacred
+        """
         self.url = url
         self.client = MongoClient(url)
         self.task = self.client['task'].task
         self.task.create_index([('_version', 1), ('_task_id', 1)], unique=True);
         self.version = version
         self.rollback = rollback
+        self.remove_failed = remove_failed
 
     def register_lock(self, _task_id, **kwargs):
         if self.version is None or self.version is False:
@@ -96,6 +105,9 @@ class task_locker:
     @contextlib.contextmanager
     def lock_block(self, task_id='Default_block', **job_paras):
         import sys
+        if self.remove_failed:
+            self.remove_failed_lock(task_id)
+
         lock_id = self.register_lock(_task_id=task_id, **job_paras, )
         if lock_id:
             try:
@@ -111,10 +123,29 @@ class task_locker:
             exist_lock = self.task.find_one({"_version": self.version,
                                                '_task_id': task_id
                                              })
-            raise Exception(f'Already had lock#{exist_lock}')
+            if exist_lock:
+                raise Exception(f'Already had lock#{exist_lock}')
+            else:
+                raise Exception(f'Can not create or find any lock')
 
 
     def remove_version(self):
         self.task.remove({'_version' : self.version})
+
+    def remove_failed_lock(self, task_id):
+
+        exist_lock = self.client.task.task.find_one({"_version": self.version, '_task_id': task_id})
+
+        try:
+            exist_task = self.client.db.runs.find_one({'config.lock_name': task_id,
+                                                       'config.version':self.version,
+                                                       'status':'FAILED'
+                                                       })
+        except Exception as e:
+            exist_task = None
+
+        if exist_lock and exist_task:
+            self.task.remove({"_version": self.version, '_task_id': task_id})
+            print(f'Remove the lock {exist_lock}, job is failed')
 
 
